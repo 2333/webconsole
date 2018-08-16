45bc15096e3553d725b834519acd02bb9cdb6e2c
BroK <zk.amaranth@gmail.com>
2018-08-17 00:16:31 +0800

    login blueprint

--- __init__.py
+++ __init__.py
@@ -4,5 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from . import auth
+    from . import auth, db
+    db.init_app(app)
     app.register_blueprint(auth.bp)
+    return app
---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP table IF exists user;
+DROP table if exists ;
+
+create table user (
+  id, integer primary key AUTOINCREMENT,
+  username TEXT,
+  qq
+)
